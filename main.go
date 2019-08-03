93023e00b07ecd8ce3b1ffc089e63188a91571b3
bsewk <jamesabaak@gmail.com>
2019-08-03 14:44:23 -0700

    Added numbers and bombs

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 type Box struct {
 	hasBomb           bool
 	neighbouringBombs byte
-	value             byte
+	value             string
 }
 
 var sizeX, sizeY, numOfBombs int16
@@ -19,32 +19,60 @@ func main() {
 	fmt.Println("Hello, world")
 	sizeX, sizeY = 25, 25
 	mineField = initMinefield(sizeX, sizeY)
+	fmt.Println()
 	printMinefield(mineField)
 }
 
 func initMinefield(sizeX, sizeY int16) [][]Box {
 	field := make([][]Box, sizeY)
-	fmt.Println()
+	// Decide where all the bombs will be
 	for i := range field {
 		field[i] = make([]Box, sizeX)
 		for j := range field[i] {
 			field[i][j] = genBox()
 		}
 	}
+	// Determine neighbouring bombs
+	for i := range field {
+		for j := range field[i] {
+			checkSurroundings(field, i, j, &(field[i][j]))
+		}
+	}
 	return field
+}
+
+func checkSurroundings(field [][]Box, y, x int, box *Box) {
+	var bombCount byte = 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			// Ensure to skip own box and outside edges
+			if !(i == 0 && j == 0) && ((x+j) >= 0 && (y+i) >= 0) && ((x+j) < len(field[0]) && (y+i) < len(field)) {
+				if field[y+i][x+j].hasBomb {
+					bombCount++
+				}
+			}
+		}
+	}
+	box.neighbouringBombs = bombCount
+	if box.hasBomb {
+		box.value = `X`
+	} else {
+		box.value = fmt.Sprintf("%d", bombCount)
+	}
 }
 
 func genBox() Box {
 	if determineBomb() {
-		return Box{true, 0, 1}
+		return Box{hasBomb: true}
+	} else {
+		return Box{}
 	}
-	return Box{}
 }
 
 func determineBomb() bool {
 	min := 0
 	max := 100
-	threshold := 50
+	threshold := 30
 	return (rand.Intn(max-min) + min) < threshold
 }
 
@@ -67,7 +95,7 @@ func printMinefield(field [][]Box) {
 			case i == -1:
 				fmt.Printf("%v_ ", strings.Repeat(" ", xLen))
 			case i >= 0:
-				fmt.Printf(" %v%d ", strings.Repeat(" ", xLen-numOfDigits(int(field[i][j].value))), field[i][j].value)
+				fmt.Printf(" %v%v ", strings.Repeat(" ", xLen-1), field[i][j].value)
 			}
 		}
 		fmt.Println()
